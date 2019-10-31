ed0f0bde30f699510964d427ebe04d1d8eeae01f
Soldado <alison.soldadoo@gmail.com>
2019-10-31 14:57:45 -0300

    Add classes of instrumented tests and unit tests

---
+++ app/src/androidTest/java/com/example/githubapifiltercourotines/RepositoryInstrumentedTest.kt
@@ -0,0 +1,13 @@
+package com.example.githubapifiltercourotines
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.runner.RunWith
+import org.koin.test.KoinTest
+
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class RepositoryInstrumentedTest : KoinTest {
+
+}
