70ed7589eaac6b48691c4effb0d940f68a4f4ac1
Soldado <alison.soldadoo@gmail.com>
2019-11-18 17:03:09 -0300

    Change unit test

--- app/src/test/java/com/example/githubapifiltercourotines/RepositoryUnitTest.kt
+++ app/src/test/java/com/example/githubapifiltercourotines/RepositoryUnitTest.kt
@@ -3,30 +3,31 @@ package com.example.githubapifiltercourotines
 import com.example.githubapifiltercourotines.config.remoteModule
 import com.example.githubapifiltercourotines.config.repositoryModule
 import com.example.githubapifiltercourotines.config.uiModule
-import com.example.githubapifiltercourotines.ui.repository.RepositoryViewModel
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
+import org.koin.dsl.module
 import org.koin.test.KoinTest
-import org.koin.test.inject
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class RepositoryUnitTest : KoinTest {
 
-    private val repositoryViewModel : RepositoryViewModel by inject()
     private val appModules by lazy {
         listOf(remoteModule, repositoryModule, uiModule)
     }
 
     @Before
-    fun setup() {}
+    fun setup() {
+        startKoin { }
+    }
 
     @After
     fun shutdown() {
+        stopKoin()
     }
 
     @Test
